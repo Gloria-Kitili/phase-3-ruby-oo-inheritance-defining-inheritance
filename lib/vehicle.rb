fa7401997ffa54353d304fa7d5b5570baf8347c6
root <gloria.kitili@student.moringaschool.com>
2023-02-20 12:47:00 +0300

    inheritance-defining-inheritance

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,24 @@
 class Vehicle
+    attr_accessor :wheel_size, :wheel_number
 
-end
+    def initialize( wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+    
+    def go 
+        'vrrrrrrrooom!'
+    end
+
+    def fill_up_tank 
+        'filling up!'
+    end
+end 
+
+
+
+# vehicle = Vehicle.new
+#  vehicle.wheel_size
+#  vehicle.wheel_number
+#  vehicle.go
+#  vehicle.fill_up_tank
