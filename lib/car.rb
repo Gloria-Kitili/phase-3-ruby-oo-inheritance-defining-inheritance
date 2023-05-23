cc49b5b17e1ab458f9a947a1181f256918317f42
root <gloria.kitili@student.moringaschool.com>
2023-05-23 22:41:20 +0300

    inheritance

--- lib/car.rb
+++ lib/car.rb
@@ -1,27 +1,10 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative './vehicle'
+ require_relative './vehicle'
 
-class Car < Vehicle
-
-    # attr_accessor :wheel_size, :wheel_number
-
-    # def initialize(wheel_size, wheel_number)
-    #     @wheel_Size = "small"
-    #     @wheel_number = "2"
-    # end
-
-     def go
-        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
-     end
+ class Car < Vehicle
+   def go
+      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     end
+end
 
-    #  def fill_up_tank
-    #     'filling up!'
-    #  end
-    # end
-
-    # car = Car.new
-    # car. wheel_size
-    # car.wheel_number
-    # car.go
-    # car.fill_up_tank
+    
